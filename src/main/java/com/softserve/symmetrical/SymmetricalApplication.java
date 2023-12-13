a7fe91e74d474c0cbc39b423d79ee24f4fc01626
Julian Misnaza <_Julian.Misnaza.ext.AZ@sodexo.com>
2023-12-12 23:06:11 -0500

    Initial commit

---
+++ src/main/java/com/softserve/symmetrical/SymmetricalApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.symmetrical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymmetricalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymmetricalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softserve/symmetrical/SymmetricalApplicationTests.java
@@ -0,0 +1,18 @@
+package com.softserve.symmetrical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymmetricalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testData(){
+        System.out.println("xxxx");
+    }
+
+}
