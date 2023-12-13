49cf3a137a85c265b91c4c9647817ad20b1e0979
Julian Misnaza <_Julian.Misnaza.ext.AZ@sodexo.com>
2023-12-13 09:51:54 -0500

    Extract to constants

--- src/main/java/com/softserve/symmetrical/EliminateDuplicateText.java
+++ src/main/java/com/softserve/symmetrical/EliminateDuplicateText.java
@@ -2,11 +2,14 @@ package com.softserve.symmetrical;
 
 public class EliminateDuplicateText {
 
+    private static final String SPACE = " ";
+    private static final String VOID_CHART = "";
+
     public static String deleteDuplicateInString(String value){
         return value.toUpperCase().chars().distinct().collect(
                 StringBuilder::new,
                 StringBuilder::appendCodePoint,
                 StringBuilder::append
-        ).toString().replaceAll(" ", "");
+        ).toString().replaceAll(SPACE, VOID_CHART);
     }
 }
